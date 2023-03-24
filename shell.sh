334f5591d65801cfdce8324c9a3ad82f8bf1f464
Jeffrey Descan <jeffrey.descan@be.sentia.com>
2023-03-24 11:49:09 +0100

    Change shell installer

--- shell.sh
+++ shell.sh
@@ -5,8 +5,8 @@
 ##
 
 # Install oh-my-zsh theme
-rm -rf $ZSH_CUSTOM/themes/powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+rm -rf ~/.dotfiles/zsh/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.dotfiles/zsh/themes/powerlevel10k
 
 # Install required fonts
 git clone https://github.com/powerline/fonts.git --depth=1 /tmp/powerline-fonts
