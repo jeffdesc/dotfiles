1e49ad5d26ddda62284482406bd72ca2ebcfba6b
Jeffrey Descan <jeffrey.descan@be.sentia.com>
2023-03-24 09:44:00 +0100

    Finetune Mac OS settings to my likings

--- keys.sh
+++ keys.sh
@@ -14,7 +14,7 @@ op item list --tags ssh --format json | op item get - --format json | jq -c | wh
   SSH_KEY=`op item get ${SSH_TITLE} --format json | jq '.fields[] | select(.id == "private_key") | .value' -r`;
   KEY_FILE="${KEYS_FOLDER}/${SSH_TITLE}.load";
   echo -e "Wrote key '$SSH_TITLE' to '${KEY_FILE}'";
-  echo "${SSH_KEY}" > ${KEY_FILE};
+  echo $SSH_KEY > ${KEY_FILE};
 done
 
 # Change the file permissions for the keys with the correct location
