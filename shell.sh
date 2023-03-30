eb30460d746fc41f8eefbd60a20bdc956b21b357
Jeffrey Descan <jeffrey.descan@be.sentia.com>
2023-03-30 11:28:26 +0200

    Fonts installed through brew

--- shell.sh
+++ shell.sh
@@ -7,10 +7,3 @@
 # Install oh-my-zsh theme
 rm -rf ~/.dotfiles/zsh/themes/powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.dotfiles/zsh/themes/powerlevel10k
-
-# Install required fonts
-git clone https://github.com/powerline/fonts.git --depth=1 /tmp/powerline-fonts
-cd /tmp/powerline-fonts
-./install.sh
-cd ../
-rm -rf /tmp/powerline-fonts
